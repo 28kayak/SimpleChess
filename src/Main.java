6b3e004532831fb4e4f56fdc997f07e40ff15d21
28kayak <kaya.ota@sjsu.edu>
2016-07-31 20:31:56 -0700

    implement moveFunction

--- src/Main.java
+++ src/Main.java
@@ -74,7 +74,7 @@ public class Main
 		 For example, if the user specifies 0, 0, and then 2,0, then (s)he wants to move the piece at location (0,0) to location (2,0), which is 2 squares vertically.
 		 c. Check for valid row and column indexes.
 		 */
-		boolean current_validity = false;
+		boolean validity = false;
 		boolean move_validity = false;
 		//check if there is a piece in the specified current position.
 		Scanner scan = new Scanner(System.in);
@@ -82,7 +82,9 @@ public class Main
 		int currentRow = scan.nextInt();
 		System.out.println("Enter the current postion of the piece for COLUMN");
 		int currentColumn = scan.nextInt();
-		if(isPiece(currentRow, currentColumn))
+		
+		
+		if(!isEmpty(currentRow, currentColumn))
 		{
 			//Yes, there is some piece
 			//So, ask where to move the piece
@@ -90,38 +92,43 @@ public class Main
 			int row_move = scan.nextInt();
 			System.out.println("Enter a move for the column");
 			int column_move = scan.nextInt();
-			
+			if((0 <= column_move && column_move < 8) &&(0 <= row_move && row_move <8))
+			{
+				//row and column are valid 
+				/*CALL makeMove()*/
+				System.out.println("===== piece is moved!!!=====");
+				validity = true;
+			}
+			else
+			{
+				System.out.println("Error: checkMoveInput()");
+				System.out.println("specified desitination is out of range");
+				validity = false;
+			}
 		}
 		else
 		{ 
 			//No piece in the specified position
-			System.out.println("ERROR: No piece in the specified position!");
+			System.out.println("ERROR: checkMoveInput() "
+					+ "\nNo piece in the specified position!");
+			validity = false;
 			
 		}
+		return validity;
 		
 		
 		
-		if(move_validity && current_validity)
-		{
-			//both true
-			System.out.println("Ready to move");
-			return true;
-			
-		}
-		else
-		{ 
-			System.out.println("Some inputs are wrong");
-			return false;
-		}
+		
 	}//CheckmoveInput
 	
 	public static void makeMove(int currentR, int currentC, int distinationR, int distinationC,String color)
 	{
-		/*A) Check if there is a piece of the correct color at the specified current location. */
+		/*A) Check if there is a piece of the correct color at the specified current location.*/
+		/*B) Check if the destination location is empty.*/
 		
 	}
 	
-	public static boolean isPiece(int currentRow, int currentColumn)
+	public static boolean isEmpty(int currentRow, int currentColumn)
 	{
 	
 		boolean validity = false;
@@ -131,12 +138,13 @@ public class Main
 			//there is no piece at the specified area 
 			System.out.println("ERROR: isPiece");
 			System.out.println("no peice at the specified position");
-			validity = false;
+			validity = true;
 			
 		}
 		else
 		{
-			validity = true;
+			//yes, there is a piece 
+			validity = false;
 		}
 	
 		
@@ -148,7 +156,7 @@ public class Main
 		String pieceColor = "";//result 
 		String[] piece;
 		
-		if(isPiece(row,column))
+		if(!isEmpty(row,column))
 		{
 			//if there is a piece, then get the piece of color
 			piece = board[row][column].split(":");
@@ -167,7 +175,7 @@ public class Main
 	{
 		String pieceName = "";
 		String[] piece;
-		if(isPiece(row, column))
+		if(!isEmpty(row, column))
 		{
 			piece = board[row][column].split(":");
 			System.out.println(piece[0]);//show name of piece 
@@ -181,12 +189,239 @@ public class Main
 		}
 		return pieceName;
 	}//getPieceName
+	public static boolean isOwnPiece(int destinationR, int destinationC,String userColor)
+	{
+		/*if true then */
+		boolean myTerritory = false;
+		String pieceColor = getPieceColor(destinationR, destinationC);
+		
+		if(pieceColor.equalsIgnoreCase(userColor))
+		{
+			//the piece is mine
+			myTerritory = true;
+		}
+		else
+		{
+			//the piece is not mine
+			myTerritory = false;
+		}
+		return myTerritory;
+	}
 	
-	/*move each type of pieces*/
-	public static void moveKing()
+	/*move each type of pieces
+	 * Precondition: destination coordinates are [0,7]
+	 * 			   : destination is empty space
+	 * */
+	
+	public static void moveKing(int currentR, int currentC,int destinationR, int destinationC)
 	{
 		
-	}
+		String color = getPieceColor(currentR,currentC);
+		if(color.equalsIgnoreCase(white))
+		{	
+			//check if required destination is valid move for black king 
+			if(currentR+1 == destinationR)
+			{	
+				//(x+1, ???)
+				//possible cases 1:(x+1, y-1)  4:(x+1, y) 6: (x+1, y+1)
+				if(currentC-1 == destinationC)
+				{//case 1 (x+1, y-1)
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else if(currentC == destinationC)
+				{//case 4 (x+1, y)
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else if(currentC + 1 == destinationC)
+				{//case 6 (x+1, y+1)
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else 
+				{
+					System.out.println("ERROR: moveKing (black)");
+					System.out.println("destination for column [ " + destinationC +" ] is invalid");
+				}
+			}
+			else if(currentR == destinationR)
+			{ //possible cases 2:(x, y -1),  7:(x, y + 1) 
+				if(currentC - 1 == destinationC)
+				{
+					//case 2
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}else if(currentC + 1 == destinationC)
+				{
+					//case 7
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else
+				{
+					System.out.println("ERROR: moveKing (black)");
+					System.out.println("destination for column [ " + destinationC +" ] is invalid");
+				}
+			}
+			else if(currentR - 1 == destinationR)
+			{//possible cases 3:(x-1, y-1), 5:(x-1, y),8:(x-1,y+1)
+				if(currentC - 1 == destinationC)
+				{
+					//case 3 
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else if(currentC == destinationC)
+				{
+					//case 5
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else if(currentC + 1 == destinationC )
+				{
+					//case 8
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:W ";
+				}
+				else
+				{
+					System.out.println("ERROR: moveKing (black)");
+					System.out.println("destination for column [ " + destinationC +" ] is invalid");
+				}
+				
+			}
+			else
+			{
+				System.out.println("ERROR: moveKing (black)");
+				System.out.println("destination for ROW [ " + destinationR +" ] is invalid");
+			}
+		}//white case
+		else if(color.equalsIgnoreCase(black))
+		{
+			System.out.println("black" + color);
+			
+			//check if required destination is valid move for black king 
+			if(currentR+1 == destinationR)
+			{	
+				//(x+1, ???)
+				//possible cases 1:(x+1, y-1)  4:(x+1, y) 6: (x+1, y+1)
+				if(currentC-1 == destinationC)
+				{//case 1 (x+1, y-1)
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else if(currentC == destinationC)
+				{//case 4 (x+1, y)
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else if(currentC + 1 == destinationC)
+				{//case 6 (x+1, y+1)
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else 
+				{
+					System.out.println("ERROR: moveKing (black)");
+					System.out.println("destination for column [ " + destinationC +" ] is invalid");
+				}
+			}//if
+			else if(currentR == destinationR)
+			{ //possible cases 2:(x, y -1),  7:(x, y + 1) 
+				if(currentC - 1 == destinationC)
+				{
+					//case 2
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}else if(currentC + 1 == destinationC)
+				{
+					//case 7
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else
+				{
+					System.out.println("ERROR: moveKing (black)");
+					System.out.println("destination for column [ " + destinationC +" ] is invalid");
+				}
+			}
+			else if(currentR - 1 == destinationR)
+			{//possible cases 3:(x-1, y-1), 5:(x-1, y),8:(x-1,y+1)
+				if(currentC - 1 == destinationC)
+				{
+					//case 3 
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else if(currentC == destinationC)
+				{
+					//case 5
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else if(currentC + 1 == destinationC )
+				{
+					//case 8
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="K:B ";
+				}
+				else
+				{
+					System.out.println("ERROR: moveKing (black)");
+					System.out.println("destination for column [ " + destinationC +" ] is invalid");
+				}
+					
+			}
+			else
+			{
+				System.out.println("ERROR: moveKing (black)");
+				System.out.println("destination for ROW [ " + destinationR +" ] is invalid");
+			}
+			
+		}
+		
+	}//moveKing
+	
+	public static void moveRook(int currentR, int currentC,int destinationR, int destinationC)
+	{
+		//Rook can move horizontally and vertically but not diagonally
+		String color = getPieceColor(currentR,currentC);
+		if(color.equalsIgnoreCase(white))
+		{
+			//if(isOwnTerritory(destinationR,destinationC,white))
+			//{
+				//System.out.println("the piece is yours. Cannot move there");
+			//}//T:isOwn(white)
+			//else
+			//{
+				if(destinationR == currentR || destinationC == currentC)
+				{
+					board[currentR][currentC] = emptySpace;
+					board[destinationR][destinationC] ="R:W ";
+				}
+				else
+				{
+					System.out.println("ERROR: moveRook (white)");
+					System.out.println("destination is invalid");
+				}
+			//}
+		}
+		else
+		{
+			//System.out.println("Black move ROOK");
+			if(destinationR == currentR || destinationC == currentC)
+			{
+				board[currentR][currentC] = emptySpace;
+				board[destinationR][destinationC] ="R:B ";
+			}
+			else
+			{
+				System.out.println("ERROR: moveRook (black)");
+				System.out.println("destination is invalid");
+			}
+		}
+	}//moveRook
 	public static void movePawn(int currentR, int currentC,int destinationR, int destinationC)
 	{
 		String color = getPieceColor(currentR,currentC);
@@ -200,14 +435,47 @@ public class Main
 			}
 			else
 			{
-				System.out.println("ERROR: movePawn");
+				System.out.println("ERROR: movePawn (White)");
 				System.out.println("Invalid destination");
 			}
 		}
+		else if(color.equalsIgnoreCase(black))
+		{//move for black pawn
+			if((currentR + 1) == destinationR && currentC == destinationC)
+			{
+				board[currentR][currentC] = emptySpace;
+				board[destinationR][destinationC] = "P:B ";
+			}
+			else
+			{
+				System.out.println("ERROR: movePawn (Black)");
+				System.out.println("Invalid destination");
+			}
+		}
+		else
+		{
+			System.out.println("Fetal Error: input user color is invalid");
+		}
+	}//movePawn
+	
+	public static void moveBishop(int currentR, int currentC, int destinationR, int destinationC)
+	{
+		//The bishop can move any number of squares diagonally.
+		String color = getPieceColor(currentR,currentC);
+		if(color.equalsIgnoreCase(white))
+		{
+			
+		}
+		else if(color.equalsIgnoreCase(black))
+		{
+			
+		}
+		else
+		{
+			
+		}
 		
-		
-		
-	}
+	}//moveBishop
 	
 	
 	public static void main(String[] args)
@@ -216,13 +484,34 @@ public class Main
 		//System.out.println("Chose your chess color from black or white");
 		//player_color = scan.next();
 		init(player_color);
-		System.out.println(isPiece(0,0));
+		
+		System.out.println(isEmpty(7,7));
+		
 		System.out.println("piece color");
 		getPieceColor(0,0);
+		
 		System.out.println("piece name");
 		getPieceName(6,3);
-		movePawn(6,3, 5,3);
+		//System.out.println("calling check Move input");
+		//System.out.println(checkMoveInput());
+		
+		
+		/*movePawn(1,3, 2,3);
 		display();
-		movePawn(6,5, 4,3);
+		movePawn(1,5, 3,5);*/
+		moveKing(7,4,6,3);
+		moveKing(0,4,1,4);
+		System.out.println("after move king");
+		display();
+		init(player_color);
+		moveRook(7,7,7,4);
+		display();
+		moveRook(0,0,3,0);
+		display();
+		//System.out.println("after move rook");
+		//moveRook(7,0,3,0);
+		//System.out.println("after move rook");
+		//display();
+		
 	}
 }
--- src/Readme.txt
+++ src/Readme.txt
@@ -1,3 +1,37 @@
+
+
+Initial Chess Board for this game:
+
+R:B N:B B:B Q:B K:B B:B N:B R:B 
+P:B P:B P:B P:B P:B P:B P:B P:B 
+x:x x:x x:x x:x x:x x:x x:x x:x 
+x:x x:x x:x x:x x:x x:x x:x x:x 
+x:x x:x x:x x:x x:x x:x x:x x:x 
+x:x x:x x:x x:x x:x x:x x:x x:x 
+P:W P:W P:W P:W P:W P:W P:W P:W 
+R:W N:W B:W Q:W K:W B:W N:W R:W 
+
+PieceName:UserColor(space)  
+x:x = no piece
+USER COLOR 
+B = black
+W = white
+
+PIECE NAME
+P = pawn
+R = rook
+N = knight 
+K = king
+Q - queen
+B = bishops 
+
+
+
+
+
+
+
+
 Worked isPiece()
 Worked getPieceName()
 Worked getPieceColor()
